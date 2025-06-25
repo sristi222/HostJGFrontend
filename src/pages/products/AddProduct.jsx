3ad8f284c9c327eff5484455f736e1b90ad5d37b
Sristi Shrestha <132660312+sristi222@users.noreply.github.com>
2025-06-25 21:54:48 +0545

    Update AddProduct.jsx

--- src/pages/products/AddProduct.jsx
+++ src/pages/products/AddProduct.jsx
@@ -180,7 +180,7 @@ function AddProductEnhanced() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/products", {
+      const res = await fetch("https://jgenterprisebackend.onrender.com/api/products", {
         method: "POST",
         body: formDataToSend,
       })
