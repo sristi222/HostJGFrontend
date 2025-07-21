739e1e66cd9ff3e17b842c716c2c3fb3eed3b93a
Sristi Shrestha <132660312+sristi222@users.noreply.github.com>
2025-07-21 12:24:36 +0545

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/categories")
+        const response = await fetch("https://jgenterprisebackend.onrender.com/api/categories")
         if (!response.ok) {
           throw new Error("Failed to fetch categories")
         }
